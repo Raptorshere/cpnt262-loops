e9fb88921c0f0a5ddeddf2cfb31fb289b09c17da
Raptorshere <Kevinfula2@gmail.com>
2023-10-24 23:07:54 -0600

    changes to js file

---
+++ client.js
@@ -0,0 +1,24 @@
+//create the variables
+
+for (let i = 0; i <= 10; i = i + 1) {
+ console.log(i)
+};
+document.querySelector("heading1").innerHTML = 
+
+    
+
+for (let i = '#'; i.length <= 10; i = i + 
+'#') {
+ console.log(i)
+};
+document.querySelector("paragraph1")
+
+
+// if and else statement (the condition)
+let picture = 3;
+
+if (picture >=6) {
+  console.log("3");
+} else {
+  console.log("3")
+}
